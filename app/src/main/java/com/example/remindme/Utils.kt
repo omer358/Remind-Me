a49eba13110fca7ebc005e36e52493c51d21dc4e
omer358 <omermaki358@gmail.com>
2020-06-29 15:00:16 +0200

    edit the fialds

--- app/src/main/java/com/example/remindme/Utils.kt
+++ app/src/main/java/com/example/remindme/Utils.kt
@@ -1,18 +1,37 @@
 package com.example.remindme
 
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val sDateFormat = SimpleDateFormat("dd MMM")
 
 
-/**
- * Returns a string representing the numeric quality rating.
- */
-fun convertStringGenderToImage(gender: String, resources: Resources): Drawable {
-    var genderImage = resources.getDrawable(R.drawable.ic_baseline_emoji_emotions_30)
-    when (gender) {
-        "Male" -> genderImage = resources.getDrawable(R.drawable.ic_baseline_emoji_emotions_30)
-        "Female" -> genderImage = resources.getDrawable(R.drawable.ic_baseline_sentiment_very_satisfied_30)
+private const val MINUTE_MILLIS = 1000 * 60.toLong()
+private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+private const val DAY_MILLIS = 24 * HOUR_MILLIS
+
+fun convertDateToPassedTime(time: String): CharSequence? {
+    val sdf = SimpleDateFormat.getDateTimeInstance()
+    val date = sdf.parse(time)
+    val millis: Long = date.time
+    val now = System.currentTimeMillis()
+
+
+    var rightTime:String
+    rightTime = if (now - millis < DAY_MILLIS){
+        if (now - millis < HOUR_MILLIS){
+            val minutes = Math.round((now - millis).toDouble())/ MINUTE_MILLIS
+            minutes.toString()+"M"
+        }else{
+            val minutes: Long =  Math.round(((now - millis) / HOUR_MILLIS).toDouble());
+            minutes.toString()+"H"
+        }
+    }else  {
+        val dateDate = Date(millis);
+        sDateFormat.format(dateDate);
 
     }
-    return genderImage
+    // Add a dot to the date string
+    rightTime = "\u2022 $date";
+    return rightTime
 }
